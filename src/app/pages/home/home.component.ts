d5360307481761fa3053c906d60979845048bc55
Vu Dang <danglegiavu@gmail.com>
2021-12-29 15:36:15 +0700

    update home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,52 @@ import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table
 import { MatSort, Sort } from '@angular/material/sort'
 import { MatPaginator } from '@angular/material/paginator';
 import { HotelService } from '../../shared/services/hotel.service';
-import { DataSource } from '@angular/cdk/collections';
+import { DataSource, SelectionModel } from '@angular/cdk/collections';
 import { IHotel } from '../../shared/models/hotel.model';
 import { first } from 'rxjs';
 import { API_ENDPOINT } from '../../shared/config/api.config';
+
+const listHotelsMock=[
+  {
+    address: "2135 Truong Van Cu Street, Quận 1, TP. Hồ Chí Minh",
+    category: null,
+    city: null,
+    description: "Hotel Hoa Saigon",
+    facilities: [],
+    hotelName: "Hotel Hoa Saigon",
+    id: 20,
+    imageUrl: "http://localhost:3000/public/assets/images/BIM_3871-Edit-990x590-2012.jpeg",
+    isActive: false,
+    rooms: [],
+  },
+  {
+    address: "23135 Truong Van Cu Street, Quận 1, TP. Hồ Chí Minh",
+    category: null,
+    city: null,
+    description: "Hotel Hoa Saigon",
+    facilities: [],
+    hotelName: "Hotel Hoa Saigon",
+    id: 20,
+    imageUrl: "http://localhost:3000/public/assets/images/BIM_3871-Edit-990x590-2012.jpeg",
+    isActive: false,
+    rooms: [],
+  },
+  {
+    address: "2A35 Truong Van Cu Street, Quận 1, TP. Hồ Chí Minh",
+    category: null,
+    city: null,
+    description: "Hotel Hoa Saigon",
+    facilities: [],
+    hotelName: "Hotel Hoa Saigon",
+    id: 20,
+    imageUrl: "http://localhost:3000/public/assets/images/BIM_3871-Edit-990x590-2012.jpeg",
+    isActive: false,
+    rooms: [],
+  },
+
+
+] 
+
 
 @Component({
   selector: 'app-home',
@@ -15,56 +57,14 @@ import { API_ENDPOINT } from '../../shared/config/api.config';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatPaginator) paginator!: MatPaginator
-  public dataSource! : MatTableDataSource<IHotel>
-
-  displayedColumns: string[] = ['id', 'imageUrl', 'hotelName','description', 'address'];
-
-  constructor(
-    private readonly _liveAnnouncer: LiveAnnouncer,
-    private readonly _hotelService: HotelService
-  ) { 
-
-  }
-
+  public mockHotel = listHotelsMock
   ngOnInit(): void {
-    this._hotelService.getAllHotels().pipe(first()).subscribe(
-      item => {
-        //item.forEach((hotel : IHotel) => hotel.imageUrl = `${API_ENDPOINT}/${hotel.imageUrl}`) as any
-        item.forEach((hotel : IHotel) => {
-          hotel.imageUrl = `${API_ENDPOINT}/${hotel.imageUrl}`,
-          hotel.description = hotel.description.slice(0,30)
-        }) 
+      
+  }
+  constructor(){}
 
-        this.dataSource = new MatTableDataSource(item)
-        this.dataSource.sort = this.sort;
-        this.dataSource.paginator = this.paginator;
-      },
-      err => console.log(err)
-    );
+  toggleHotel(hotel : IHotel){
+    console.log(hotel)
   }
 
-  /** Announce the change in sort state for assistive technology. */
-  announceSortChange(sortState: Sort | any) {
-    console.log(sortState)
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
-    if (sortState!.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState!.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
-
-  search($event: any) {
-    // Calls elasticsearch API for searching product 
-    // in production context. 
-    // But in this sample demo
-    //  I will use simple filtering :))
-
-
-  }
 }
